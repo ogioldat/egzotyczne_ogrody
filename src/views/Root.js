3ccd93d0c69730be4be91e9c9220c06503d82eb9
ogioldat <tomasz.ogiolda00@gmail.com>
2020-05-01 17:36:52 +0200

    Root route based component lazy load

--- src/views/Root.js
+++ src/views/Root.js
@@ -6,14 +6,14 @@ import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-d
 import { AnimatePresence } from 'framer-motion';
 import MainTemplate from 'templates/MainTemplate';
 import GalleryView from 'views/GalleryView';
-import {
-  OrdersView,
-  ContactView, LocationView, PolicyView,
-} from 'views/Subpages';
 
 const HomeView = lazy(() => import('views/HomeView'));
 const PlantDetailsView = lazy(() => import('views/PlantDetailsView'));
-const AboutUsView = lazy(() => import('views/AboutUsView'));
+const AboutUsView = lazy(() => import('views/Subpages/AboutUsView'));
+const OrdersView = lazy(() => import('views/Subpages/OrdersView'));
+const ContactView = lazy(() => import('views/Subpages/ContactView'));
+const LocationView = lazy(() => import('views/Subpages/LocationView'));
+const PolicyView = lazy(() => import('views/Subpages/PolicyView'));
 
 const Root = () => (
   <Provider store={ store }>
@@ -21,17 +21,19 @@ const Root = () => (
       <Route render={ ({ location }) => (
         <AnimatePresence exitBeforeEnter initial={ false }>
           <MainTemplate path={ location.pathname }>
-            <Switch location={ location }>
-              <Route exact path={ routes.home } component={ HomeView }/>
-              <Route exact path={ routes.root } render={ () => <Redirect to={ routes.home }/> }/>
-              <Route path={ routes.plantDetails } component={ PlantDetailsView }/>
-              <Route path={ routes.aboutUs } component={ AboutUsView }/>
-              <Route path={ routes.orders } component={ OrdersView }/>
-              <Route path={ routes.contact } component={ ContactView }/>
-              <Route path={ routes.location } component={ LocationView }/>
-              <Route path={ routes.gallery } component={ GalleryView }/>
-              <Route path={ routes.policy } component={ PolicyView }/>
-            </Switch>
+            <Suspense fallback={<div>loading</div>}>
+              <Switch location={ location }>
+                <Route exact path={ routes.home } component={ HomeView }/>
+                <Route exact path={ routes.root } render={ () => <Redirect to={ routes.home }/> }/>
+                <Route path={ routes.plantDetails } component={ PlantDetailsView }/>
+                <Route path={ routes.aboutUs } component={ AboutUsView }/>
+                <Route path={ routes.orders } component={ OrdersView }/>
+                <Route path={ routes.contact } component={ ContactView }/>
+                <Route path={ routes.location } component={ LocationView }/>
+                <Route path={ routes.policy } component={ PolicyView }/>
+                <Route path={ routes.gallery } component={ GalleryView }/>
+              </Switch>
+            </Suspense>
           </MainTemplate>
         </AnimatePresence>
       ) }/>
