fca8f85ed625e95f9aa8bfd4ca5d98ca07cccac3
ogioldat <tomasz.ogiolda00@gmail.com>
2020-05-14 15:53:08 +0200

    read more button

--- src/components/molecules/ShowMore/ShowMore.js
+++ src/components/molecules/ShowMore/ShowMore.js
@@ -1,22 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Paragraph from '../../atoms/Paragraph/Paragraph';
 
 const StyledWrapper = styled.div`
-  width: 80vw;
-  height: 80vh;
-  background-color: white;
+  position: relative;
+  height: 15vh;
+  overflow: hidden;
 `;
 
-const ShowMore = ({ text }) => (
-  <StyledWrapper>
-    <Paragraph>
-      {
-        text
-      }
-    </Paragraph>
-  </StyledWrapper>
-);
+const StyledReadMore = styled.div`
+  border-radius: 6px;
+  position: absolute;
+  display: inline-block;
+  background-color: ${ ({ theme }) => theme.greyDark };
+  color: ${ ({ theme }) => theme.greyLight };
+  font: normal small-caps bold ${ ({ theme }) => theme.fontSize.xs } Montserrat, serif-sans;
+  width: 150px;
+  text-align: center;
+  padding: 5px 15px;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+`;
 
+const StyledShader = styled.div`
+  position: absolute;
+  height: 100px;
+  bottom: 0;
+  width: 100%;
+  background: linear-gradient(rgba(255,255,255,0), ${ ({ theme }) => theme.greyLight });
+`;
+
+
+const ShowMore = ({ text }) => {
+  const [isExpanded, expand] = useState(false);
+
+  return (
+    <StyledWrapper isExpanded={ isExpanded }>
+      <Paragraph>
+        {
+          text
+        }
+      </Paragraph>
+      <StyledShader/>
+      <StyledReadMore>CZYTAJ WIĘCEJ</StyledReadMore>
+    </StyledWrapper>
+  );
+
+};
 export default ShowMore;
 
